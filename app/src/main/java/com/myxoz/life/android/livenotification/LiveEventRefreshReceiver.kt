f2dc2b4afb287cfb7b82168f4b98cf782d44ba29
myxoz <github@myxoz.de>
2026-09-13 09:03:11 +0200

    Fixed Live event

--- app/src/main/java/com/myxoz/life/android/livenotification/LiveEventRefreshReceiver.kt
+++ app/src/main/java/com/myxoz/life/android/livenotification/LiveEventRefreshReceiver.kt
@@ -3,7 +3,9 @@ package com.myxoz.life.android.livenotification
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import com.myxoz.life.android.MainApplication
+import com.myxoz.life.android.livenotification.LiveRescheduling.Companion.LOGTAG
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -14,9 +16,12 @@ class LiveEventRefreshReceiver: BroadcastReceiver() {
 
         CoroutineScope(Dispatchers.IO).launch {
             try {
-                (context.applicationContext as MainApplication)
-                    .liveRescheduling
-                    .launchRefresh()
+                Log.d(LOGTAG, "Received LiveRescheduling")
+                (context.applicationContext as? MainApplication)
+                    ?.liveRescheduling
+                    ?.launchRefresh()
+                    ?: Log.d(LOGTAG, "Cannot acess LiveRescheduling")
+
             } finally {
                 pendingResult.finish()
             }
